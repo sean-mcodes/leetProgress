8533d68cfe579cafb184f5303b6f32ade134dbec
sean-mcodes <105676197+sean-mcodes@users.noreply.github.com>
2023-03-05 17:50:12 -0800

    Time: 53 ms (93.81%), Space: 42.2 MB (7.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -6,16 +6,17 @@
 
 var climbStairs = function(n) {
 
- let dp = [1,1];
- 
+  let dp = Array(n).fill(0);
     
-  for (let i = 2; i <= n; i++ ) {
-    let result = dp[0] + dp[1];
-     dp[0] = dp[1]
-     dp[1] = result 
+  dp[0] = 1;
+  dp[1] = 1;
+  dp[2] = 2;  
+    
+  for (let i = 3; i <= n; i++ ) {
+    dp[i] = dp[i - 1] + dp[i - 2];     
   }
 
-  return dp[1]    
+  return dp[n]    
 };
 
    
