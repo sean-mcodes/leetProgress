6d17414751eb14df0665d48adecd09b23e3bcd23
sean-mcodes <105676197+sean-mcodes@users.noreply.github.com>
2023-04-20 16:14:49 -0700

    Time: 66 ms (20.21%), Space: 41.7 MB (78.50%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,11 +3,13 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    const words = [];
-    //split string into array
-    const array = s.split(' ')
-    for (i of array) { if (i.length > 0) words.push(i) }
-    // console.log(words)
-    //return the length of the last index
-    return words[words.length - 1].length
+    // const words = [];
+    // //split string into array
+    // const array = s.split(' ')
+    // for (i of array) { if (i.length > 0) words.push(i) }
+    // //return the length of the last index
+    // return words[words.length - 1].length
+    
+    const array = s.trim().split(' ')
+    return array[array.length - 1].length
 };
