86acc0a813db3a3c03310bb99aa409b02f0cc260
sean-mcodes <105676197+sean-mcodes@users.noreply.github.com>
2023-05-05 14:50:40 -0700

    Time: 74 ms (25.23%), Space: 44.8 MB (9.04%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,25 +11,42 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function (list1, list2) {
+
+  //recurisve
+
   if (!list1) return list2;
   if (!list2) return list1;
 
-  const mergedList = new ListNode();
-  let current = mergedList;
-
-  while (list1 && list2) {
-    if (list1.val < list2.val) {
-      current.next = list1;
-      list1 = list1.next;
-    } else {
-      current.next = list2;
-      list2 = list2.next;
-    }
-      current = current.next;
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    return list1;
+  } else {
+    list2.next = mergeTwoLists(list1, list2.next)
+    return list2;
   }
 
-  if (list1) current.next = list1;
-  if (list2) current.next = list2;
+  /*iterative
 
-  return mergedList.next;
+  if (!list1) return list2;
+  if (!list2) return list1;
+
+  // const mergedList = new ListNode();
+  // let current = mergedList;
+
+  // while (list1 && list2) {
+  //   if (list1.val < list2.val) {
+  //     current.next = list1;
+  //     list1 = list1.next;
+  //   } else {
+  //     current.next = list2;
+  //     list2 = list2.next;
+  //   }
+
+  //   current = current.next;
+  // }
+
+  // if (list1) current.next = list1;
+  // if (list2) current.next = list2;
+
+  return mergedList;*/
 };
