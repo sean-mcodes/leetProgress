54bcd7d080be5695ce707b69c13b383cc3195e2a
sean-mcodes <105676197+sean-mcodes@users.noreply.github.com>
2023-05-05 12:13:25 -0700

    Time: 128 ms (31.43%), Space: 57.3 MB (11.93%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  //create cache to store counts of characters
+  const cache = {};
+  //iterate through array param
+  for (let i = 0; i < nums.length; i++) {
+    //count each occurrence of each number
+    if (cache[nums[i]] === undefined) {
+      cache[nums[i]] = 1
+    } else {
+      cache[nums[i]]++
+    }
+  }
+
+  //iterate through cache
+  for (let key in cache) {
+    if (cache[key] > 1) return true;
+  }
+
+  return false;
+};
+
